46f8e626bd30299bdb7638c6621d3b23191c51cf
Thomas <thomas-1999@web.de>
2020-05-26 13:48:34 +0200

    added some values for bill object

--- app/src/main/java/database/billData.kt
+++ app/src/main/java/database/billData.kt
@@ -4,12 +4,26 @@ package database
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity(tableName = "bills")
 class billData {
 
         @PrimaryKey
         var id:Int = 0
+
+        var address:String = ""
+
+        var name:String = ""
+
+        var item:String = ""
+
+        var amount:Int = 0
+
+        var salestax:Int = 0
+
+        var time:Date? = null
+
 
 }
 
