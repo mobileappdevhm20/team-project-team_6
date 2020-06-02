dd9e501e24bed1705176785f9ee6b8cd16964c61
Salman Alhadziev <salman.alhadziev0@hm.edu>
2020-06-02 20:22:44 +0200

    added scan button click listener

--- app/src/main/java/com/example/easybill/ScanFragment.kt
+++ app/src/main/java/com/example/easybill/ScanFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.easybill.databinding.ScanBinding
 
 /**
@@ -22,6 +24,13 @@ class ScanFragment : Fragment() {
         val binding :ScanBinding = DataBindingUtil.inflate(
             inflater, R.layout.scan, container, false
         )
+
+        binding.scanButton.setOnClickListener {
+            // TODO scan bill and add to database
+            // TODO check if bill already exists in database
+            it.findNavController().navigate(R.id.action_scanFragment_to_archiveFragment)
+        }
+
         return binding.root
     }
 
