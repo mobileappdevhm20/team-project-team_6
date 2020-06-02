57b20dc883223deb0bc3373610cff45dff181e57
Salman Alhadziev <salman.alhadziev0@hm.edu>
2020-06-02 09:21:51 +0200

    removed unused code from main activity

--- app/src/main/java/com/example/easybill/MainActivity.kt
+++ app/src/main/java/com/example/easybill/MainActivity.kt
@@ -2,15 +2,8 @@ package com.example.easybill
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import kotlinx.android.synthetic.main.archiv.*
 
 class MainActivity : AppCompatActivity() {
-
-    val testCompany = arrayOf<String>("Oberpollinger","Saturn","H&M")
-    val testDate = arrayOf<String>("27.04.2020","04.10.2019","05.08.2019")
-    val testPrice = arrayOf<String>("295.00","539.00","74.98")
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
