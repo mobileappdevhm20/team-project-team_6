b5b9c730804595d0e27168b8f72883edfc2c910b
salman2893 <salman.alhadziev0@hm.edu>
2020-06-23 22:44:01 +0200

    Feature/take photo (#65)
    * added picture container backgrounds
    
    * added gitignore
    
    * added android image cropper dependency
    
    * followed instructions in android image cropper guide
    
    * added some colors for the picture container backgrounds
    
    * added required permissions
    
    * edited add bill layout to display picture etc.
    
    * implemented the feature of taking a picture of a bill
    
    * adjusted gitignore
    
    * fixed lint

--- app/src/main/java/com/easybill/ui/addbill/AddBillFragment.kt
+++ app/src/main/java/com/easybill/ui/addbill/AddBillFragment.kt
@@ -1,19 +1,32 @@
 package com.easybill.ui.addbill
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.easybill.MainViewModel
 import com.easybill.R
 import com.easybill.misc.generateFakeBills
+import com.theartofdev.edmodo.cropper.CropImage
+import kotlinx.android.synthetic.main.fragment_add.view.*
 
 class AddBillFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
+
+    private lateinit var photoViewContainer: LinearLayout
+    private lateinit var photoView: ImageView
+    private lateinit var scanButton: Button
+    private lateinit var confirmButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +55,73 @@ class AddBillFragment : Fragment() {
                 viewModel.addBill(bill)
         }
 
+        val cameraButton: Button = root.findViewById(R.id.camera_button)
+        cameraButton.setOnClickListener { openCamera() }
+
+        scanButton = root.findViewById(R.id.scan_button)
+        scanButton.setOnClickListener { scanPhoto() }
+
+        confirmButton = root.findViewById(R.id.confirm_button)
+        confirmButton.setOnClickListener { confirmPhoto() }
+
+        photoView = root.photo_image_view
+        photoViewContainer = root.photo_view_container
+
+        setInitialViewsState()
+
         return root
+    }
+
+    private fun setInitialViewsState() {
+        scanButton.isEnabled = false
+        confirmButton.isEnabled = false
+        photoViewContainer.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.bill_picture_container_init))
+        photoView.setImageURI(null)
+    }
+
+    private fun openCamera() {
+        CropImage.activity()
+            .setActivityTitle("Crop Photo")
+            .setAllowFlipping(false)
+            .start(requireContext(), this)
+        setInitialViewsState()
+    }
+
+    private fun scanPhoto() {
+        // TODO use OCR to scan the photo
+        // TODO check if scan was successful
+        val scanSuccessful = true
+        if (scanSuccessful) {
+            photoViewContainer.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.bill_picture_container_success))
+            scanButton.isEnabled = false
+            confirmButton.isEnabled = true
+        } else {
+            photoViewContainer.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.bill_picture_container_fail))
+            Toast.makeText(this.activity, "Scan failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun confirmPhoto() {
+        // TODO confirm and save the bill
+        Toast.makeText(this.activity, "Bill added", Toast.LENGTH_SHORT).show()
+        setInitialViewsState()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                val result = CropImage.getActivityResult(data)
+                if (resultCode == Activity.RESULT_OK) {
+                    photoView.setImageURI(result.uri)
+                    photoViewContainer.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.bill_picture_container))
+                    scanButton.isEnabled = true
+                }
+            }
+            else -> {
+                Toast.makeText(this.activity, "Unrecognized request code", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
