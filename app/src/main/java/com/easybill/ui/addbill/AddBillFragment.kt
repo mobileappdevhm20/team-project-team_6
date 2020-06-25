d64539415c920af15c542132cf3bb5cc6d56f697
Salman Alhadziev <salman.alhadziev0@hm.edu>
2020-06-25 23:11:48 +0200

    first implementation of ocr

--- app/src/main/java/com/easybill/ui/addbill/AddBillFragment.kt
+++ app/src/main/java/com/easybill/ui/addbill/AddBillFragment.kt
@@ -3,6 +3,9 @@ package com.easybill.ui.addbill
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
@@ -12,22 +15,28 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageProxy
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.easybill.MainViewModel
 import com.easybill.R
 import com.easybill.misc.generateFakeBills
-import com.google.android.gms.vision.Frame
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.TextRecognizer
 import com.theartofdev.edmodo.cropper.CropImage
 import kotlinx.android.synthetic.main.fragment_add.view.*
 import java.io.IOException
 
+
+/**
+ * Uses the Android-Image-Cropper library to take a picture and crop it
+ * Source: https://github.com/ArthurHub/Android-Image-Cropper
+ * Uses the ML Kit to process a picture of a bill
+ * Source: https://developers.google.com/ml-kit/vision/text-recognition/android#top_of_page
+ */
 class AddBillFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
@@ -111,6 +120,7 @@ class AddBillFragment : Fragment() {
                 photoViewContainer.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.bill_picture_container_success))
                 scanButton.isEnabled = false
                 confirmButton.isEnabled = true
+                processBillText(visionText)
             }
             .addOnFailureListener { e ->
                 // Task failed with an exception
@@ -126,7 +136,7 @@ class AddBillFragment : Fragment() {
 
     private fun confirmPhoto() {
         // TODO confirm and save the bill
-        Toast.makeText(this.activity, "Bill added", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this.activity, "Bill added (not really)", Toast.LENGTH_LONG).show()
         setInitialViewsState()
     }
 
@@ -138,7 +148,7 @@ class AddBillFragment : Fragment() {
                 val result = CropImage.getActivityResult(data)
                 if (resultCode == Activity.RESULT_OK) {
                     photoView.setImageURI(result.uri)
-                    //val bitmap: Bitmap = MediaStore.Images.Media.getBitmap(activity?.contentResolver, result.uri)
+                    imageBitmap = MediaStore.Images.Media.getBitmap(activity?.contentResolver, result.uri)
                     //photoView.setImageBitmap(bitmap)
                     try {
                         image = InputImage.fromFilePath(requireContext(), result.uri)
@@ -155,5 +165,148 @@ class AddBillFragment : Fragment() {
                 Toast.makeText(this.activity, "Unrecognized request code", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    class BillValueEntry(
+        val text: String,
+        val textX: Float,
+        val textY: Float,
+        val textSize: Float,
+        var left: Float,
+        val top: Float,
+        val right: Float,
+        val bottom: Float)
+
+    private fun processBillText(visionText: Text) {
+
+        val headLine = "#".repeat(50)
+        val subLine = "-".repeat(50)
+
+        var regexForTotal: MutableList<String> = mutableListOf(".*total.*", ".*sum.*", ".*summe.*")
+        var regexForAddress: MutableList<String> = mutableListOf(".*straße.*", ".*str.*", ".*street.*")
+
+        val shopNameBlock = visionText.textBlocks[0]
+        var shopName: BillValueEntry = BillValueEntry(
+            text = shopNameBlock.lines[0].text,
+            textX = shopNameBlock.lines[0].boundingBox?.left!!.toFloat(),
+            textY = shopNameBlock.lines[0].boundingBox?.bottom!!.toFloat(),
+            textSize = shopNameBlock.lines[0].boundingBox?.bottom!!.toFloat() - shopNameBlock.lines[0].boundingBox?.top!!.toFloat(),
+            left = shopNameBlock.boundingBox?.left!!.toFloat(),
+            right = shopNameBlock.boundingBox?.right!!.toFloat(),
+            top = shopNameBlock.boundingBox?.top!!.toFloat(),
+            bottom = shopNameBlock.boundingBox?.bottom!!.toFloat()
+        )
+
+        var addressList: MutableList<BillValueEntry>
+        var totalMap: MutableMap<BillValueEntry, BillValueEntry>
+        var otherEntries: MutableList<BillValueEntry> = mutableListOf()
+
+        println(headLine)
+
+        for (block in visionText.textBlocks) {
+            // Get values --------------------------------------------------------------------------
+            val blockText = block.text
+            val blockFrame = block.boundingBox
+            // Print -------------------------------------------------------------------------------
+            println("\nBlock text: $blockText")
+            println("Frame:\n\tLeft: ${blockFrame?.left}\n\tRight: ${blockFrame?.right}\n\tTop: ${blockFrame?.top}\n\tBottom: ${blockFrame?.bottom}")
+            println(subLine)
+            // -------------------------------------------------------------------------------------
+            for (line in block.lines) {
+                val lineText = line.text
+                val lineFrame = line.boundingBox
+                // ----------
+                otherEntries.add(BillValueEntry(
+                    text = lineText,
+                    textX = lineFrame?.left!!.toFloat(),
+                    textY = lineFrame?.bottom!!.toFloat(),
+                    textSize = lineFrame?.bottom!!.toFloat() - lineFrame?.top!!.toFloat(),
+                    left = blockFrame?.left!!.toFloat(),
+                    top = blockFrame?.top!!.toFloat(),
+                    right = blockFrame?.right!!.toFloat(),
+                    bottom = blockFrame?.bottom!!.toFloat()
+                ))
+                // ----------
+            }
+        }
+
+        println(headLine)
+        drawBillContentToPicture(shopName, null, null, null, otherEntries)
+    }
+
+    private fun drawBillContentToPicture(
+        shopName: BillValueEntry?,
+        address: BillValueEntry?,
+        total: BillValueEntry?,
+        totalValue: BillValueEntry?,
+        other: MutableList<BillValueEntry>?)
+    {
+        var bitmap: Bitmap = Bitmap.createBitmap(imageBitmap.width,imageBitmap.height, Bitmap.Config.RGB_565)
+        val billCanvas = Canvas(bitmap)
+
+        billCanvas.drawBitmap(imageBitmap, 0f, 0f, null)
+
+        val shopNameColor = ResourcesCompat.getColor(resources, R.color.colorGreenSuccess, null)
+        val addressColor = ResourcesCompat.getColor(resources, R.color.colorRedFail, null)
+        val totalColor = ResourcesCompat.getColor(resources, R.color.colorPrimary, null)
+        val otherColor = ResourcesCompat.getColor(resources, R.color.colorLightGrey, null)
+
+        val paint = Paint().apply {
+            // Smooths out edges of what is drawn without affecting shape.
+            isAntiAlias = true
+            // Dithering affects how colors with higher-precision than the device are down-sampled.
+            isDither = true
+            style = Paint.Style.STROKE // default: FILL
+            strokeJoin = Paint.Join.ROUND // default: MITER
+            strokeCap = Paint.Cap.ROUND // default: BUTT
+            strokeWidth = 5f // default: Hairline-width (really thin)
+        }
+
+        // draw all other
+        if (other != null) {
+            paint.color = otherColor
+            for (otherEntry in other) {
+                paint.style = Paint.Style.STROKE
+                billCanvas.drawRoundRect(RectF(otherEntry.left, otherEntry.top, otherEntry.right, otherEntry.bottom), 0f, 0f, paint)
+                paint.style = Paint.Style.FILL
+                paint.textSize = otherEntry.textSize
+                billCanvas.drawText(otherEntry.text, otherEntry.textX, otherEntry.textY, paint)
+            }
+        }
+
+        // draw shop name
+        if (shopName != null) {
+            paint.color = shopNameColor
+            paint.style = Paint.Style.STROKE
+            billCanvas.drawRoundRect(RectF(shopName.left, shopName.top, shopName.right, shopName.bottom), 0f, 0f, paint)
+            paint.style = Paint.Style.FILL
+            paint.textSize = shopName.textSize
+            billCanvas.drawText(shopName.text, shopName.textX, shopName.textY, paint)
+        }
+
+        // draw address if not null
+        if (address != null) {
+            paint.color = addressColor
+            paint.style = Paint.Style.STROKE
+            billCanvas.drawRoundRect(RectF(address.left, address.top, address.right, address.bottom), 0f, 0f, paint)
+            paint.style = Paint.Style.FILL
+            paint.textSize = address.textSize
+            billCanvas.drawText(address.text, address.textX, address.textY, paint)
+        }
+
+        // draw total
+        if (total != null && totalValue != null) {
+            paint.color = totalColor
+            paint.style = Paint.Style.STROKE
+            billCanvas.drawRoundRect(RectF(total.left, total.top, total.right, total.bottom), 0f, 0f, paint)
+            billCanvas.drawRoundRect(RectF(totalValue.left, totalValue.top, totalValue.right, totalValue.bottom), 0f, 0f, paint)
+            paint.style = Paint.Style.FILL
+            paint.textSize = total.textSize
+            billCanvas.drawText(total.text, total.textX, total.textY, paint)
+            paint.textSize = totalValue.textSize
+            billCanvas.drawText(totalValue.text, totalValue.textX, totalValue.textY, paint)
+        }
+
+        photoView.setImageBitmap(bitmap)
     }
 }
